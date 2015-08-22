c1c9b94d293856932bacd5bafa722b6afae798ba
KvltKitty <jherzenach@mail.csuchico.edu>
2015-08-22 05:10:27 -0700

    Basic Water Animation
    Script waterMove.cs just modifies the shader's texture offset x and y to
    move it

---
+++ Zen/Assets/Scripts/World/waterMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterMove : MonoBehaviour {
+	public Renderer rend;
+	float curOffsetX, curOffsetY;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.material.shader = Shader.Find("Standard");
+		curOffsetX = 0.0f;
+		curOffsetY = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		curOffsetX += 0.002f;
+		curOffsetY += 0.0005f;
+		if(curOffsetX >= 1.0f){
+			curOffsetX = 0.0f;
+		}
+		if(curOffsetY >= 1.0f){
+			curOffsetX = 0.0f;
+		}
+		rend.material.SetTextureOffset("_MainTex", new Vector2(curOffsetX, -curOffsetY));
+	}
+}
